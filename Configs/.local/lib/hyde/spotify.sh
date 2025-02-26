9af8ac9ad0a5cd319e6adab2836bc9a05de36df4
sitolam <otis.lammertyn@gmail.com>
2025-02-26 14:00:02 +0100

    feat: add spotify script

---
+++ Configs/.local/lib/hyde/spotify.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+
+# set variables
+
+scrDir=`dirname "$(realpath "$0")"`
+source $scrDir/globalcontrol.sh
+
+if [ $# -eq 0 ] ; then
+    echo "usage: ./spotify.sh <spotify-title>"
+    exit 1
+else
+    spotifyTitle="$1"
+fi
+
+
+hyprctl dispatch focusworkspaceoncurrentmonitor 10
+
+if [ -z "$(hyprctl clients | grep "$spotifyTitle" | grep -v "grep" | grep -v "./spotify.sh")" ]; then
+    hyprctl dispatch movecursor 0 0 
+
+    kitty --title "spotify cava" sh -c "exit"
+    kitty --title "spotify cava" sh -c "cava" &
+    sleep 0.5
+    hyprctl --batch "dispatch focuswindow 'initialtitle:(spotify cava)' ; dispatch resizeactive exact 50% 55% ;  dispatch centerwindow 1 ; dispatch moveactive 70% 60%"
+    
+
+    spotify &
+    sleep 1
+    until [ ! -z "$(hyprctl clients | grep "$spotifyTitle" | grep -v "grep" | grep -v "./spotify.sh")" ] 
+    do
+     echo "" 
+    done
+    hyprctl --batch "dispatch focuswindow 'initialtitle:($spotifyTitle)' ; dispatch resizeactive exact 50% 55% ; dispatch centerwindow 1 ; dispatch moveactive -70% -60%" 
+    sleep 3
+    playerctl play-pause
+fi
